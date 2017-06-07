3f4a003ac391580f4e5e4556a1425f877a7fe65d
Anabel Ramirez and Jun Fritz <anabel+jun@>
2017-06-07 07:44:25 -0700

    add initial files, run dnu restore and add pseudocode for notes.

---
+++ Objects/Cuisine.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace RestaurantProject
+{
+  public class Cuisine
+  {
+
+    // SIMILAR TO "CATEGORY" CLASS
+    // WILL INCLUDE AUTOMATICALLY GENERATED ID
+    // WILL INCLUDE NAME/TYPE OF CUISINE FOR THE "RESTAURANT" CLASS TO REFERENCE
+
+  }
+}
---
+++ Objects/Restaurant.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace RestaurantProject
+{
+  public class Restaurant
+  {
+
+    //SIMILAR TO TASKS CLASS
+    //WILL INCLUDE AUTOMATICALLY GENERATED INDIVIDUAL ID
+    //WILL MATCH "CUISINE" ID AND ONLY INCLUDE ONE CUISINE
+    //WILL INCLUDE
+      _restaurantName (STRING),
+      _rating (INT) to store rating 0-100, 
+      _availability(BOOL) to check for open or close at this moment,
+      _ownerOfRestaurant(BOOL) to check for "admin"
+
+  }
+}
