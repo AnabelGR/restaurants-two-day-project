f55d9229b3345e0b94d83acc2d996f6b3534459a
Anabel Ramirez and Jun Fritz <anabel+jun@>
2017-06-07 12:53:05 -0700

    add owner functionality and OwnerTest to test CheckPassword function.

---
+++ Tests/OwnerTest.cs
@@ -0,0 +1,24 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace RestaurantProject
+{
+  public class OwnerTest
+  {
+    [Fact]
+    public void TestCheckPasswordFunction_ReturnFalse()
+    {
+      Owner newOwner = new Owner("Jun Fritz", "password");
+      Assert.Equal(false, newOwner.CheckPassword());
+    }
+    [Fact]
+    public void TestCheckPasswordFunction_ReturnTrue()
+    {
+      Owner newOwner = new Owner("admin", "pa55w0rd");
+      Assert.Equal(true, newOwner.CheckPassword());
+    }
+  }
+}
